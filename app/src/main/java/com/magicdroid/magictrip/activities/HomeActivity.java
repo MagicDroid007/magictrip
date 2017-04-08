4e928c6d4ff06ac513e12fbff914eb461a25cb59
Sanidhya Kumar <sanidhya.kumar@timesgroup.com>
2017-04-08 14:59:00 +0530

    serach toolbar

--- app/src/main/java/com/magicdroid/magictrip/activities/HomeActivity.java
+++ app/src/main/java/com/magicdroid/magictrip/activities/HomeActivity.java
@@ -1,19 +1,90 @@
 package com.magicdroid.magictrip.activities;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.magicdroid.magictrip.R;
-import com.magicdroid.magictrip.network.APIServices;
-import com.magicdroid.magictrip.network.RetroFitServiceCreator;
+import com.quinny898.library.persistentsearch.SearchBox;
+import com.quinny898.library.persistentsearch.SearchResult;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HomeActivity extends BaseActivity {
+
+    private SearchBox search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        search = (SearchBox) findViewById(R.id.searchbox);
+        for (int x = 0; x < 10; x++) {
+            SearchResult option = new SearchResult("Result " + Integer.toString(x), getResources().getDrawable(R.drawable.ic_action_mic));
+            search.addSearchable(option);
+        }
+        search.enableVoiceRecognition(this);
+        search.setLogoText("Search destinations...");
+        search.setMenuListener(new SearchBox.MenuListener() {
+
+            @Override
+            public void onMenuClick() {
+                //Hamburger has been clicked
+                Toast.makeText(mContext, "Menu click", Toast.LENGTH_LONG).show();
+            }
+
+        });
+        search.setSearchListener(new SearchBox.SearchListener() {
+
+            @Override
+            public void onSearchOpened() {
+                //Use this to tint the screen
+            }
+
+            @Override
+            public void onSearchClosed() {
+                //Use this to un-tint the screen
+            }
+
+            @Override
+            public void onSearchTermChanged(String s) {
+
+            }
+
+            @Override
+            public void onSearch(String searchTerm) {
+                Toast.makeText(mContext, searchTerm + " Searched", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onResultClick(SearchResult result) {
+                //React to a result being clicked
+            }
 
 
+            @Override
+            public void onSearchCleared() {
+
+            }
+
+        });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SearchBox.VOICE_RECOGNITION_CODE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data
+                    .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            search.populateEditText(matches.get(0));
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
+
